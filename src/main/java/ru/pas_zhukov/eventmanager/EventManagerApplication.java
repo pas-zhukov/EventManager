7148cef08ddf3c0fc776579c709133e5e6b6e3a0
pas-zhukov <pas-zhukov@yandex.ru>
2024-10-27 18:55:33 +0300

    initial

---
+++ src/main/java/ru/pas_zhukov/eventmanager/EventManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.pas_zhukov.eventmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagerApplication.class, args);
+    }
+
+}
