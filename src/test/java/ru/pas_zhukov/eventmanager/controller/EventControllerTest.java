975403258bb71068c65301fe008157b6da0a2e97
pas-zhukov <pas-zhukov@yandex.ru>
2024-11-14 22:27:45 +0300

    база тестов евентов

---
+++ src/test/java/ru/pas_zhukov/eventmanager/controller/EventControllerTest.java
@@ -0,0 +1,40 @@
+package ru.pas_zhukov.eventmanager.controller;
+
+import ru.pas_zhukov.eventmanager.TestInContainer;
+
+public class EventControllerTest extends TestInContainer {
+
+    public void shouldSuccessCreateEvent() {
+
+    }
+
+    public void shouldNotCreateEventOnInvalidRequest() {
+
+    }
+
+    public void shouldSuccessDeleteEvent() {
+
+    }
+
+    public void shouldNotDeleteEventWhenRequestIsNotFromOwnerOrAdmin() {
+
+    }
+
+    public void shouldSuccessOnGetEventById() {
+
+    }
+
+    public void shouldReturnNotFoundOnGetNonExistingEventById() {
+
+    }
+
+    public void shouldSuccessOnUpdateEvent() {
+
+    }
+
+    public void shouldNotUpdateEventOnInvalidRequest() {
+
+    }
+
+
+}
